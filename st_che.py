ddec42d2bfcd1142218f34409ab65b17e9f27b36
Surabhi3009 <73625674+Surabhi3009@users.noreply.github.com>
2022-08-04 23:18:43 +0530

    Add files via upload

---
+++ st_che.py
@@ -0,0 +1,47 @@
+import qspr
+import streamlit as st
+
+option = st.selectbox(
+     'How would you like to give input?',
+     ('cmpd name', 'SMILES'))
+
+st.write('You selected:', option)
+
+def display(f):
+    l=["First zagreb index ",'Randic index ','Recipocal Randic index ',
+       'Second zagreb index ',
+       'Forgetten Topological index ','Atom -bond connectivity index',
+       'Augemented Zagreb index ','Geometric - Arithmetic index ','Sum connectivity index ']
+    
+    st.subheader("Degree based Topological Indices")
+    for i in range(0,len(l)):
+      st.write(l[i],f[1][i+1])
+    
+try:    
+    if option=="cmpd name":
+        c= st.text_input("Enter cmpd name")
+        #st.write("Hello", your_name)
+        #c=smiles_to_iupac(c)
+        #st.success("Success")
+
+        c=qspr.CIRconvert(c)
+        f=qspr.topological_ind(c)
+        st.subheader("Molecular graph")
+        st.pyplot(fig=f[0])
+        display(f)
+        
+    else:
+        c= st.text_input("Enter SMILES")
+        #st.write("Hello", your_name)
+        #c=smiles_to_iupac(c)
+        #c=CIRconvert(c)
+        #st.success("Success")
+
+        r=qspr.topological_ind(c)
+        st.subheader("Molecular graph")
+        st.pyplot(fig=r[0])
+        dispaly(r)
+except AttributeError:
+    pass
+        
+        
